c47f38782ed13087f25889790c8e6c6ca27624ed
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-13 14:48:35 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+main.remove(); 
+
+
+const newHeader = document.createElement("h1")
+
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Ian is the champion";
